5b9d93b5c6082c44599fd82bef28f03c295834f2
Aaron Powell <me@aaron-powell.com>
2021-05-13 14:29:45 +1000

    making scrollbars work

--- src/components/HorizontalScroller/styles.js
+++ src/components/HorizontalScroller/styles.js
@@ -1,24 +1,23 @@
 import styled from "styled-components";
 
 export const ScrollContainer = styled.div.attrs(() => ({
-	className: "scroller horizontal-scroller",
+  className: "scroller horizontal-scroller",
 }))`
-	display: flex;
-	flex-wrap: nowrap;
-	padding-left: 49px;
-	overflow-x: auto;
-	-webkit-overflow-scrolling: touch;
-	white-space: nowrap;
-	width: 100%;
+  display: flex;
+  flex-wrap: nowrap;
+  padding-left: 49px;
+  -webkit-overflow-scrolling: touch;
+  white-space: nowrap;
+  width: 100%;
 
-	::-webkit-scrollbar {
-		display: none;
-	}
+  ::-webkit-scrollbar {
+    display: none;
+  }
 
-	&::after {
-		content: "";
-		display: block;
-		flex: 0 0 20px;
-		width: 20px;
-	}
+  &::after {
+    content: "";
+    display: block;
+    flex: 0 0 20px;
+    width: 20px;
+  }
 `;
